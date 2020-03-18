e7b71ce611cbc2056708addc4266df62c56ea9b6
Guilherme Ribas <guilherme.ribas.tech@gmail.com>
2020-03-18 12:02:02 -0300

    Update store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,3 @@
-// import { createStore } from 'redux';
-
-// import rootReducer from './modules/rootReducer';
-
-// const enhancer =
-//   process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
-
-// const store = createStore(rootReducer, enhancer);
-
-// export default store;
-
 import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
